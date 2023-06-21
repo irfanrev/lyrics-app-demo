f7fcce3c042d6874004b0b3fffa6ac7a84a82440
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-06-22 00:14:00 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './Home';
+import Lyrics from './components/Lyrics';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path={'/'} element={<Home />} />
+                <Route path={'/search/lyrics/:id'} element={<Lyrics />} />
+            </Routes>
+            <Footer />
+        </Router>
   );
 }
 
